5821fc9858a5c702cb070738e287858dac2d6913
jfcorreas <jfcorreas@gmail.com>
2020-10-17 21:06:35 +0200

    Adjust Canny detection low threshold parameter

--- canny_detector.py
+++ canny_detector.py
@@ -1,33 +1,26 @@
 from __future__ import print_function
 import cv2 as cv
-import argparse
 
-max_lowThreshold = 100
+max_lowThreshold = 300
 window_name = 'Edge Map'
 title_trackbar = 'Min Threshold:'
 ratio = 3
 kernel_size = 3
 
-def main():
-    def CannyThreshold(val):
+
+def canny_image(src):
+    def canny_threshold(val):
         low_threshold = val
-        img_blur = cv.blur(src_gray, (3, 3))
-        detected_edges = cv.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
-        mask = detected_edges != 0
-        dst = src * (mask[:, :, None].astype(src.dtype))
-        cv.imshow(window_name, dst)
+        img_blur = cv.blur(src, (3, 3))
+        detected_edges = cv.Canny(src, low_threshold, low_threshold * ratio, kernel_size)
+        #mask = detected_edges != 0
+        #dst = src * (mask[:, :, None].astype(src.dtype))
+        cv.imshow(window_name, detected_edges)
+        return detected_edges
 
-    img_path = "yoshi.png"
-    src = cv.imread(img_path, cv.IMREAD_COLOR)
-    src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
     cv.namedWindow(window_name)
-    cv.createTrackbar(title_trackbar, window_name, 0, max_lowThreshold, CannyThreshold)
-    CannyThreshold(0)
+    cv.createTrackbar(title_trackbar, window_name, 0, max_lowThreshold, canny_threshold)
+    canny_img = canny_threshold(0)
     cv.waitKey()
+    return canny_img
 
-
-
-
-
-if __name__ == '__main__':
-    main()
--- colorlabeler.py
+++ colorlabeler.py
@@ -7,6 +7,8 @@ from skimage.color import rgb2lab, deltaE_cie76
 import matplotlib
 import matplotlib.pyplot as plt
 
+from canny_detector import canny_image
+
 image_colors = None
 
 
@@ -24,7 +26,6 @@ def replace_black_color(img):
 
 
 def opening_colors(img, radius: int):
-
     combined_mask = 0
 
     for c in image_colors:
@@ -60,9 +61,9 @@ def show_colors_list(img):
     x = 0
     y = 500
     for c in extract_image_colors(img):
-        rgb_color = (int(c[2]), int(c[1]), int(c[0]))   # B, G, R conversion
-        cv2.rectangle(whiteblankimage, pt1=(x, y), pt2=(x+100, y-100), color=rgb_color, thickness=-1)
-        cv2.putText(whiteblankimage, f"{rgb_color}", (x+5, y-40),
+        rgb_color = (int(c[2]), int(c[1]), int(c[0]))  # B, G, R conversion
+        cv2.rectangle(whiteblankimage, pt1=(x, y), pt2=(x + 100, y - 100), color=rgb_color, thickness=-1)
+        cv2.putText(whiteblankimage, f"{rgb_color}", (x + 5, y - 40),
                     fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4,
                     color=(200, 250, 200), thickness=1)
         if x < 400:
@@ -89,68 +90,39 @@ def pick_color(event, x, y, flags, param):
         cv2.imshow("mask", image_mask)
 
 
-def find_grid(img, low_threshold=0, ratio=33, kernel_size=3):
-
-    filter = False
-
-    img_blur = cv2.blur(img, (3, 3))
-    kernel = np.ones((3, 3), np.uint8)
-    img_blur = cv2.dilate(img_blur, kernel, iterations=1)
-    edges = cv2.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
-
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #edges = cv2.Canny(gray, 90, 150, apertureSize=3)
-    cv2.imshow('canny1.jpg', edges)
-    kernel = np.ones((3, 3), np.uint8)
-    edges = cv2.dilate(edges, kernel, iterations=1)
-    cv2.imshow('canny2.jpg', edges)
-    kernel = np.ones((5, 5), np.uint8)
-    edges = cv2.erode(edges, kernel, iterations=1)
-    cv2.imshow('canny3.jpg', edges)
-
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
-
-    if not lines.any():
-        print('No lines were found')
-        exit()
-
-    if filter:
-        rho_threshold = 75
-        theta_threshold = 0.5
-
-        # how many lines are similar to a given one
-        similar_lines = {i: [] for i in range(len(lines))}
-        for i in range(len(lines)):
-            for j in range(len(lines)):
-                if i == j:
-                    continue
-
-                rho_i, theta_i = lines[i][0]
-                rho_j, theta_j = lines[j][0]
-                if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
-                    similar_lines[i].append(j)
-
-        # ordering the INDECES of the lines by how many are similar to them
-        indices = [i for i in range(len(lines))]
-        indices.sort(key=lambda x: len(similar_lines[x]))
-
-        # line flags is the base for the filtering
-        line_flags = len(lines) * [True]
-        for i in range(len(lines) - 1):
-            if not line_flags[indices[i]]:  # if we already disregarded the ith element in the ordered list then we don't care (we will not delete anything based on it and we will never reconsider using this line again)
+def filter_lines(lines, rho_threshold=40, theta_threshold=0.5):
+    # how many lines are similar to a given one
+    similar_lines = {i: [] for i in range(len(lines))}
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            if i == j:
                 continue
 
-            for j in range(i + 1, len(lines)):  # we are only considering those elements that had less similar line
-                if not line_flags[indices[j]]:  # and only if we have not disregarded them already
-                    continue
+            rho_i, theta_i = lines[i][0]
+            rho_j, theta_j = lines[j][0]
+            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+                similar_lines[i].append(j)
 
-                rho_i, theta_i = lines[indices[i]][0]
-                rho_j, theta_j = lines[indices[j]][0]
-                if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
-                    line_flags[
-                        indices[j]] = False  # if it is similar and have not been disregarded yet then drop it now
+    # ordering the INDECES of the lines by how many are similar to them
+    indices = [i for i in range(len(lines))]
+    indices.sort(key=lambda x: len(similar_lines[x]))
 
-    print('number of Hough lines:', len(lines))
+    # line flags is the base for the filtering
+    line_flags = len(lines) * [True]
+    for i in range(len(lines) - 1):
+        if not line_flags[indices[i]]:  # if we already disregarded the ith element in the ordered list then we don't
+            # care (we will not delete anything based on it and we will never reconsider using this line again)
+            continue
+
+        for j in range(i + 1, len(lines)):  # we are only considering those elements that had less similar line
+            if not line_flags[indices[j]]:  # and only if we have not disregarded them already
+                continue
+
+            rho_i, theta_i = lines[indices[i]][0]
+            rho_j, theta_j = lines[indices[j]][0]
+            if abs(rho_i - rho_j) < rho_threshold and abs(theta_i - theta_j) < theta_threshold:
+                line_flags[
+                    indices[j]] = False  # if it is similar and have not been disregarded yet then drop it now
 
     filtered_lines = []
 
@@ -158,12 +130,41 @@ def find_grid(img, low_threshold=0, ratio=33, kernel_size=3):
         for i in range(len(lines)):  # filtering
             if line_flags[i]:
                 filtered_lines.append(lines[i])
-    else:
-        filtered_lines = lines
+    return filtered_lines
+
+
+def find_grid(img, low_threshold=0, ratio=33, kernel_size=3):
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = canny_image(gray)
+    #edges = cv2.Canny(gray, 90, 270, apertureSize=3)
+    kernel = np.ones((3, 3), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=2)
+    cv2.imshow('canny2.jpg', edges)
+    kernel = np.ones((4, 4), np.uint8)
+    edges = cv2.erode(edges, kernel, iterations=2)
+    cv2.imshow('canny3.jpg', edges)
+    cv2.waitKey()
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 150)
+
+    if not lines.any():
+        print('No lines were found')
+        exit()
+
+    print('number of Hough lines:', len(lines))
+
+    #filtered_lines = lines
+    filtered_lines = filter_lines(lines)
 
     grid_pattern = 255 * np.ones(shape=[img.shape[0], img.shape[1], 3], dtype=np.uint8)
 
     print('Number of filtered lines:', len(filtered_lines))
+    # Draw the lines
+    #if filtered_lines is not None:
+    #    for i in range(0, len(filtered_lines)):
+    #        l = filtered_lines[i][0]
+    #        cv2.line(grid_pattern, (l[0]+1000, l[1]), (l[2]-1000, l[3]), (0, 0, 0), 3, 2)
+
     if filtered_lines is not None:
         for i in range(0, len(filtered_lines)):
             rho = lines[i][0][0]
@@ -172,29 +173,27 @@ def find_grid(img, low_threshold=0, ratio=33, kernel_size=3):
             b = np.sin(theta)
             x0 = a * rho
             y0 = b * rho
-            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
-            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * (a)))
+            pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * (a)))
             cv2.line(grid_pattern, pt1, pt2, (0, 0, 0), 3, 2)
 
     return grid_pattern
 
 
 def find_grid_contours(grid):
-
     gray = cv2.cvtColor(grid, cv2.COLOR_BGR2GRAY)
     retval, threshed = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
 
     contours, h = cv2.findContours(threshed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    #dst1 = 255 * np.ones(shape=[grid.shape[0], grid.shape[1], 3], dtype=np.uint8)
-    #cv2.drawContours(dst1, contours, -1, (0, 255, 0), 1)
-    #cv2.imshow("Grid contours", dst1)
+    # dst1 = 255 * np.ones(shape=[grid.shape[0], grid.shape[1], 3], dtype=np.uint8)
+    # cv2.drawContours(dst1, contours, -1, (0, 255, 0), 1)
+    # cv2.imshow("Grid contours", dst1)
 
     return contours
 
 
 def mark_image_colors(grid, img, color_symbols):
-
     contours = find_grid_contours(grid)
     for cnt in contours:
         rect = cv2.boundingRect(cnt)
@@ -217,24 +216,23 @@ def generate_color_symbol_dict(image_colors):
 
 
 def main():
-
     global image_colors
 
-    img_path = "yoshi.png"
+    img_path = "mario.png"
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
     img_without_black = replace_black_color(img)
     cv2.imshow("image without black", img_without_black)
-    #cv2.waitKey(0)
+    # cv2.waitKey(0)
 
     image_colors = extract_image_colors(img_without_black)
     opened_img = opening_colors(img_without_black, 3)
-    #cv2.imshow("opened_image", opened_img)
+    # cv2.imshow("opened_image", opened_img)
 
     image_colors = extract_image_colors(opened_img)
     unified_img = unify_similar_colors(opened_img, 4)
-    #cv2.imshow("unified_image", unified_img)
-    #cv2.waitKey(0)
+    # cv2.imshow("unified_image", unified_img)
+    # cv2.waitKey(0)
 
     grid_pattern = find_grid(unified_img)
     cv2.imshow("Grid Pattern", grid_pattern)
@@ -244,17 +242,15 @@ def main():
     analyzed_grid = mark_image_colors(grid_pattern, unified_img, color_symbols)
     cv2.imshow("Analyzed Grid Pattern", analyzed_grid)
 
-    #show_colors_list(img_cv)
-    show_colors_list(unified_img)
 
+    #show_colors_list(unified_img)
 
-    #cv2.namedWindow('unified_image')
-    #cv2.setMouseCallback('unified_image', pick_color)
+    # cv2.namedWindow('unified_image')
+    # cv2.setMouseCallback('unified_image', pick_color)
 
     # now click into the hsv img , and look at values:
-    #image_colors = cv2.cvtColor(unified_img, cv2.COLOR_BGR2RGB)
-    #cv2.imshow("unified_image", image_colors)
-
+    # image_colors = cv2.cvtColor(unified_img, cv2.COLOR_BGR2RGB)
+    # cv2.imshow("unified_image", image_colors)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -262,4 +258,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
