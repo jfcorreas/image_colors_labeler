6fb8265b87ee2910e5917fa3d72170d227dbea29
jfcorreas <jfcorreas@gmail.com>
2020-10-17 17:03:15 +0200

    Clean code of RGB conversions

--- colorlabeler.py
+++ colorlabeler.py
@@ -1,12 +1,7 @@
-import colorsys
-import extcolors
 from PIL import Image, ImageDraw
 import cv2
 import numpy as np
-from skimage import io
 from skimage.color import rgb2lab, deltaE_cie76
-import random
-import string
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -15,28 +10,17 @@ image_colors = None  # global
 
 
 def extract_image_colors(img):
-    #colors = Image.fromarray(img).getcolors()
     all_rgb_codes = img.reshape(-1, img.shape[-1]).copy()
     colors = np.unique(all_rgb_codes, axis=0, return_counts=False)
     return colors
 
 
-def extract_image_colors_rgb(img):
-    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    #colors = Image.fromarray(rgb_image).getcolors()
-    all_rgb_codes = rgb_image.reshape(-1, rgb_image.shape[-1]).copy()
-    colors = np.unique(all_rgb_codes, axis=0, return_counts=False)
-    return colors
-
-
 def unify_similar_colors(img, colors, delta_threshold: int):
-    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    rgb_image_array = np.array(rgb_image)
+    rgb_image_array = np.array(img)
 
-    lab = rgb2lab(rgb_image)
+    lab = rgb2lab(img)
 
     for color in colors:
-        #color_3d = np.uint8(np.asarray([[color[1]]]))
         color_3d = np.uint8(np.asarray([[color]]))
         dE_color = deltaE_cie76(rgb2lab(color_3d), lab)
         rgb_image_array[dE_color < delta_threshold] = color_3d
@@ -44,7 +28,7 @@ def unify_similar_colors(img, colors, delta_threshold: int):
     rgb_image = Image.fromarray(rgb_image_array, 'RGB')
     rgb_image = np.uint8(rgb_image)
 
-    return cv2.cvtColor(rgb_image, cv2.COLOR_BGR2RGB)
+    return rgb_image
 
 
 def opening_colors(img, colors, radius: int):
@@ -52,7 +36,6 @@ def opening_colors(img, colors, radius: int):
     combined_mask = 0
 
     for c in colors:
-        #mask = cv2.inRange(img, c[1], c[1])
         mask = cv2.inRange(img, c, c)
         kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (radius, radius))
         opened_mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
@@ -68,9 +51,8 @@ def show_colors_list(img):
     whiteblankimage = 255 * np.ones(shape=[500, 500, 3], dtype=np.uint8)
     x = 0
     y = 500
-    for c in extract_image_colors_rgb(img):
-        #rgb_color = (c[1][0], c[1][1], c[1][2])
-        rgb_color = (int(c[0]), int(c[1]), int(c[2]))
+    for c in extract_image_colors(img):
+        rgb_color = (int(c[2]), int(c[1]), int(c[0]))   # B, G, R conversion
         cv2.rectangle(whiteblankimage, pt1=(x, y), pt2=(x+100, y-100), color=rgb_color, thickness=-1)
         cv2.putText(whiteblankimage, f"{rgb_color}", (x+5, y-40),
                     fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.4,
@@ -137,9 +119,9 @@ def mark_image_colors(grid, img, color_symbols):
         x, y, w, h = rect
         pixel = img[y + int(h / 2), x + int(w / 2)]
         col = (int(pixel[0]), int(pixel[1]), int(pixel[2]))
-        cv2.putText(grid, color_symbols[tuple(col)], (x + int(w / 2), y + int(h / 2)),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.3,
-                    color=tuple(col), thickness=2)
+        cv2.putText(grid, color_symbols[tuple(col)], (x + 10, y + 15),
+                    fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.5,
+                    color=tuple(col), thickness=1)
     return grid
 
 
@@ -227,49 +209,19 @@ def find_grid(img):
     return grid_pattern
 
 
-def get_squares(img):
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow('grayImage', gray_image)
-    (imgThresh, blackAndWhiteImage) = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    cv2.imshow('b&wImage', blackAndWhiteImage)
-    cv2.waitKey(0)
-
-    contours, hierarchy = cv2.findContours(blackAndWhiteImage.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    print("Contours found: %d", len(contours))
-
-    arrrect = []
-    #img_area = cv2.contourArea(contours)
-
-    for cnt in contours:
-        epsilon = 0.01 * cv2.arcLength(cnt, True)
-        approx = cv2.approxPolyDP(cnt, epsilon, False)
-        area = cv2.contourArea(approx)
-        rect = cv2.minAreaRect(approx)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-       # percentage = (area * 100) / img_area
-        #if percentage > 0.3:
-        arrrect.append(box)
-
-    #img = cv2.?????????(img, arrrect, "Green", 10)
-    cv2.drawContours(img, img, -1, (0, 255, 0), thickness=1)
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow("image", img)
-
-
 def replace_black_color(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV)
     img[mask == 255] = [5, 5, 5]
     return img
 
 
 def generate_color_symbol_dict(image_colors):
-    chars = list(string.ascii_uppercase[0:len(image_colors)])
+    symbols = list("AXOT#5PVB9$º=@2LN/+")
     color_symbol_dict = {}
     for color in image_colors:
         c = (color[0], color[1], color[2])
-        color_symbol_dict[tuple(c)] = chars.pop()
+        color_symbol_dict[tuple(c)] = symbols.pop()
     return color_symbol_dict
 
 
@@ -281,25 +233,28 @@ def main():
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
     img_without_black = replace_black_color(img)
+    cv2.imshow("image without black", img_without_black)
+    cv2.waitKey(0)
 
     image_colors = extract_image_colors(img_without_black)
     opened_img = opening_colors(img_without_black, image_colors, 3)
     cv2.imshow("opened_image", opened_img)
 
-    image_colors = extract_image_colors_rgb(opened_img)
+    image_colors = extract_image_colors(opened_img)
     unified_img = unify_similar_colors(opened_img, image_colors, 4)
     cv2.imshow("unified_image", unified_img)
     cv2.waitKey(0)
 
     grid_pattern = find_grid(unified_img)
+    cv2.imshow("Grid Pattern", grid_pattern)
 
     image_colors = extract_image_colors(unified_img)
     color_symbols = generate_color_symbol_dict(image_colors)
-    print(color_symbols)
     analyzed_grid = mark_image_colors(grid_pattern, unified_img, color_symbols)
+    cv2.imshow("Analyzed Grid Pattern", analyzed_grid)
 
     #show_colors_list(img_cv)
-    #show_colors_list(unified_img)
+    show_colors_list(unified_img)
 
 
     #cv2.namedWindow('unified_image')
@@ -309,7 +264,6 @@ def main():
     #image_colors = cv2.cvtColor(unified_img, cv2.COLOR_BGR2RGB)
     #cv2.imshow("unified_image", image_colors)
 
-    cv2.imshow("Analyzed Grid Pattern", analyzed_grid)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
