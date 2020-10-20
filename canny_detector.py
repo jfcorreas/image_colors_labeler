b2f6091d8bce13946013ee66fdf5dfc73e14ff7b
jfcorreas <jfcorreas@gmail.com>
2020-10-20 18:39:35 +0200

    Canny detector threshold graphic adjust

--- canny_detector.py
+++ canny_detector.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import cv2 as cv
 
 max_lowThreshold = 300
@@ -6,21 +5,31 @@ window_name = 'Edge Map'
 title_trackbar = 'Min Threshold:'
 ratio = 3
 kernel_size = 3
+canny_img = None
+src_img = None
+
+
+def canny_threshold(val):
+    global canny_img
+    global src_img
+
+    low_threshold = val
+    img_blur = cv.blur(src_img, (3, 3))
+    canny_img = cv.Canny(img_blur, low_threshold, low_threshold * ratio, kernel_size)
+    cv.imshow(window_name, canny_img)
 
 
 def canny_image(src):
-    def canny_threshold(val):
-        low_threshold = val
-        img_blur = cv.blur(src, (3, 3))
-        detected_edges = cv.Canny(src, low_threshold, low_threshold * ratio, kernel_size)
-        #mask = detected_edges != 0
-        #dst = src * (mask[:, :, None].astype(src.dtype))
-        cv.imshow(window_name, detected_edges)
-        return detected_edges
+    global canny_img
+    global src_img
 
+    canny_img = src.copy()
+    src_img = src.copy()
     cv.namedWindow(window_name)
     cv.createTrackbar(title_trackbar, window_name, 0, max_lowThreshold, canny_threshold)
-    canny_img = canny_threshold(0)
+    canny_threshold(0)
     cv.waitKey()
     return canny_img
+
+
 
